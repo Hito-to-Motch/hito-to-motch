d7df9dbd8fdb860fcb03730fee098d98aeac5bc1
SuzukiDaishi <zukky.rikugame@gmail.com>
2021-11-14 09:25:51 +0900

    create: loginの作成

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,5 +1,32 @@
 <template>
   <section class="section">
     サインイン
+    <input type="email" v-model="email" />
+    <input type="password" v-model="password" />
+    <button @click="signin">サインイン</button>
   </section>
 </template>
+
+<script>
+import firebase from '~/plugins/firebase.js'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async signin() {
+      try {
+        const { user, } = await firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+        this.$store.commit('setUser', user)
+        console.log(user)
+      } catch(e) {
+        alert("失敗:", e.message)
+      }
+    }
+  }
+}
+</script>
